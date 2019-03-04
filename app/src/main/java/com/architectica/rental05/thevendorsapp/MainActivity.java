a7b1ec4b3d03a5a185c396446915aae8140e7c21
sanjay147654 <sanjay14765423@gmail.com>
2019-03-04 12:54:45 +0530

    login problem

--- app/src/main/java/com/architectica/rental05/thevendorsapp/MainActivity.java
+++ app/src/main/java/com/architectica/rental05/thevendorsapp/MainActivity.java
@@ -2,23 +2,32 @@ package com.architectica.rental05.thevendorsapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.iid.FirebaseInstanceId;
+
+import org.json.JSONObject;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -28,29 +37,33 @@ import static com.architectica.rental05.thevendorsapp.FirstRunSecondActivity.use
 public class MainActivity extends AppCompatActivity {
 
     private ImageView mOyo;
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     static {
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
     }
 
-    static String deviceTokenId;
     static boolean isMainActivity;
+    static String deviceTokenId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("MainActivity","yes");
-
         isMainActivity = true;
 
-        mOyo = (ImageView) findViewById(R.id.oyo_main) ;
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        Log.d("MainActivity", "yes");
+
+        mOyo = (ImageView) findViewById(R.id.oyo_main);
         Boolean isFirstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE)
                 .getBoolean("isFirstRun", true);
 
         ConnectivityManager cm =
-                (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+                (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
 
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
         boolean isConnected = activeNetwork != null &&
@@ -60,9 +73,20 @@ public class MainActivity extends AppCompatActivity {
         //mOyo.setTypeface(custom_font);
         //mOyo.setLetterSpacing(-0.03f);
 
+        //the app is not run for the first time
+        //check if there is a saved auth state
+
+        userUid = getSharedPreferences("User", MODE_PRIVATE)
+                .getString("UserUid", null);
+
+        FirstRunSecondActivity.vendorName = getSharedPreferences("VendorName", MODE_PRIVATE)
+                .getString("Name", null);
+
         if (isConnected) {
 
             //internet connection is there
+
+            FirebaseDatabase.getInstance().goOnline();
 
             if (isFirstRun) {
 
@@ -213,13 +237,14 @@ public class MainActivity extends AppCompatActivity {
 
                 }
 
+
             }
-        }
-        else {
+        } else {
 
             //internet connection is not there
             Toast.makeText(this, "No internet connection.Please check your network and try again", Toast.LENGTH_SHORT).show();
         }
+
     }
 }
 
